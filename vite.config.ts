29e8bc9da143cb397a9bef69c63851f1cd34b5ac
utsav raj <22054218@kiit.ac.in>
2025-05-29 21:53:44 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-    include: ['react-simple-typewriter']
-  },
-  resolve: {
-    dedupe: ['react', 'react-dom', 'react-simple-typewriter']
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'typewriter': ['react-simple-typewriter']
+        }
+      }
+    }
   }
 });
